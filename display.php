8f2da05e607e5800a9ee25b2ad55d2c34af42242
¨siranjivi-it19¨ <siranjivi.it19@bitsathy.ac.in>
2019-08-21 13:47:16 +0530

    firstcommit

---
+++ display.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Display</title>
+</head>
+<body>
+<?php
+echo"<b><center> Hello </font></b> <br> This is my first php program </center>";
+$txt="Hai";
+echo"<center>$txt</center>";
+$a="1";
+$b="2";
+$c=$a+$b;
+echo"<center> $a+$b=$c </center>";
+?>
+</body>
+</html>
---
+++ print.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Numbers</title>
+</head>
+<body>
+<?php
+for($i=1;$i<=10;$i++)
+{
+echo"The number is: $i<br>";
+}
+?>
+</body>
+</html> 
+
